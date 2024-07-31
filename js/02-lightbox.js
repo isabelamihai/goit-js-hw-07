f88eaebf5b631ecfc75501b8f22179f6ee171c40
isabelamihai <isabelaelenamihai@gmail.com>
2024-08-01 00:15:44 +0300

    Update
    JS-07-EX1-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+// Funcție pentru crearea marcajului galeriei
+function createGalleryMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+  }).join('');
+}
+
+// Crearea și adăugarea marcajului în containerul galeriei
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.innerHTML = galleryMarkup;
+
+// Inițializarea librăriei SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
